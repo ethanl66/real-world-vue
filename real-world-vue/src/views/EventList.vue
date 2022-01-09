db610d5f3590dfa88b049019f355641270c4294d
Ehan <ethanliang405@gmail.com>
2022-01-09 18:01:10 -0500

    API Calls with Axios: TERNARY OPERATOR TO FIX .LENGTH BEFORE API DATA IS LOADED

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <event-card />
+    <event-card v-for="event in events" :key="event.id" :event="event" />
     <base-icon />
   </div>
 </template>
@@ -9,11 +9,26 @@
 <script>
 import EventCard from '@/components/EventCard.vue'
 import BaseIcon from '../components/BaseIcon.vue'
+import EventService from '@/services/EventService.js'
 
 export default {
   components: {
     EventCard,
     BaseIcon,
+  },
+  data() {
+    return {
+      events: [],
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data
+      })
+      .catch((error) => {
+        console.log('There was an error:' + error.response)
+      })
   },
 }
 </script>
