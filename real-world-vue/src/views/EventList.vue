09b430a8a2a6b7f13a8655f2508a6e80a4ab0b02
Ehan <ethanliang405@gmail.com>
2022-01-09 17:27:31 -0500

    L7 Global COmponents

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -2,15 +2,18 @@
   <div>
     <h1>Events Listing</h1>
     <event-card />
+    <base-icon />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import BaseIcon from '../components/BaseIcon.vue'
 
 export default {
   components: {
     EventCard,
+    BaseIcon,
   },
 }
 </script>
